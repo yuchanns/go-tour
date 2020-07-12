07dc971434983456c0bd202b8a1dfcc8edfd8f66
yuchanns <airamusume@gmail.com>
2020-07-12 22:30:47 +0800

    add word internal

--- cmd/word.go
+++ cmd/word.go
@@ -1,12 +1,58 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/yuchanns/go-tour/internal/word"
+	"log"
+	"strings"
+)
+
+const (
+	MODE_UPPER = iota + 1
+	MODE_LOWER
+	MODE_UNDERSCORE_TO_UPPER_CAMELCASE
+	MODE_UNDERSCORE_TO_LOWER_CAMELCASE
+	MODE_CAMELCASE_TO_UNDERSORE
+)
+
+var str string
+var mode int8
+
+var desc = strings.Join([]string{
+	"该子命令支持各种单次格式转换，模式如下：",
+	"1：全部单词转为大写",
+	"2：全部单词转为小写",
+	"3：下划线单词转为大写驼峰单词",
+	"4：下划线单词转为小写驼峰单词",
+	"5：驼峰单词转为下划线单词",
+}, "\n")
+
+func init() {
+	wordCmd.Flags().StringVarP(&str, "str", "s", "", "请输入单词内容")
+	wordCmd.Flags().Int8VarP(&mode, "mode", "m", 0, "请输入单词转换模式")
+}
 
 var wordCmd = &cobra.Command{
 	Use:   "word",
 	Short: "单词格式转换",
-	Long:  "支持多种单词格式转换",
+	Long:  desc,
 	Run: func(cmd *cobra.Command, args []string) {
-		//
+		var content string
+		switch mode {
+		case MODE_UPPER:
+			content = word.ToUpper(str)
+		case MODE_LOWER:
+			content = word.ToLower(str)
+		case MODE_UNDERSCORE_TO_UPPER_CAMELCASE:
+			content = word.UnderscoreToUpperCamelCase(str)
+		case MODE_UNDERSCORE_TO_LOWER_CAMELCASE:
+			content = word.UnderscoreToLowerCamelCase(str)
+		case MODE_CAMELCASE_TO_UNDERSORE:
+			content = word.CamelCaseToUnderscore(str)
+		default:
+			log.Fatalln("暂不支持该模式转换，执行help word查看帮助文档")
+		}
+		fmt.Printf("输出结果： %s\n", content)
 	},
 }
