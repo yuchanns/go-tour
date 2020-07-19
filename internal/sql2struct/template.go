2c51d59ba21d5f6e1758e0e73749185988118110
yuchanns <airamusume@gmail.com>
2020-07-19 18:21:12 +0800

    fix

--- internal/sql2struct/template.go
+++ internal/sql2struct/template.go
@@ -12,8 +12,7 @@ import (
 
 const StructTpl = `
 type {{.TableName | ToCamelCase}} struct {
-{{range .Columns}}{{$length := len .Comment}}{{if gt $length 0}}// {{.Comment}}{{else}}// {{.Name}}{{end}}
-    {{$typeLen := len .Type}}{{if gt $typeLen 0}}{{.Name | ToCamelCase}}{{.Type}}{{.Tag}}{{else}}{{.Name}}{{end}}
+{{range .Columns}}{{$typeLen := len .Type}}{{if gt $typeLen 0}}{{.Name | ToCamelCase}} {{.Type}}{{.Tag}}{{else}}{{.Name}}{{end}} {{$length := len .Comment}}{{if gt $length 0}}// {{.Comment}}{{else}}// {{.Name}}{{end}}
 {{end}}}
 
 func (model {{.TableName | ToCamelCase}}) TableName() string {
