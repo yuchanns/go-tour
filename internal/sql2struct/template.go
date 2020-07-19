847f9ad23a6a6d4ad71b15062b8a02a7f3d3497a
yuchanns <airamusume@gmail.com>
2020-07-19 17:30:21 +0800

    fix:template indent

--- internal/sql2struct/template.go
+++ internal/sql2struct/template.go
@@ -8,12 +8,12 @@ import (
 )
 
 const StructTpl = `type {{.TableName | ToCamelCase}} struct {
-{{range .Columns}} {{$length := len .Comment}}    {{if gt $length 0}}// {{.Comment}} {{else}}// {{.Name}} {{end}}
-    {{$typeLen := len .Type}} {{if gt $typeLen 0}}{{.Name | ToCamelCase}} {{.Type}}    {{.Tag}}{{else}}{{.Name}}{{end}}
+{{range .Columns}}{{$length := len .Comment}}    {{if gt $length 0}}// {{.Comment}}{{else}}// {{.Name}} {{end}}
+    {{$typeLen := len .Type}}{{if gt $typeLen 0}}{{.Name | ToCamelCase}} {{.Type}}    {{.Tag}}{{else}}{{.Name}}{{end}}
 {{end}}}
 
 func (model {{.TableName | ToCamelCase}}) TableName() string {
-	return "{{.TableName}}"
+    return "{{.TableName}}"
 }
 `
 
